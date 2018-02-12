ddc62b6ef6e941b6ae1f431ad26172e103d3fc1d
Shink Man <cshinkle@iastate.edu>
2018-02-11 20:44:02 -0600

    Getting started on adding history

--- ComS 352/project1/Shell.c
+++ ComS 352/project1/Shell.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 //#include <stdlib.h>
-//#include <unistd.h>
+#include <unistd.h>
 #define MAX_LINE 80
 
-char *trim(char *str) {
-  char *end;
-
-  // Trim leading space
-  while(isspace((unsigned char)*str)) str++;
-
-  if(*str == 0)  // All spaces?
-    return str;
-
-  // Trim trailing space
-  end = str + strlen(str) - 1;
-  while(end > str && isspace((unsigned char)*end)) end--;
-
-  // Write new null terminator
-  *(end+1) = 0;
-  
-  return str;
+void removeNewlineChar(char *word) {
+  word[strlen(word)-1] = 0;
 }
 
-int  parseInput(char *line, char **args) {
+int parseInput(char *line, char **args) {
   char *token = strtok(line, " ");
   int i;
   for (i=0;token && i < MAX_LINE/2;i++) {
     args[i] = token;
     token = strtok(NULL, " ");
   }
+  removeNewlineChar(args[i-1]);
   args[i] = NULL;
   return 0;
 }
@@ -56,9 +42,25 @@ int main(void)
       fprintf(stderr, "Something went wrong with parsing input...\n");
       should_run = 0;
     }
-    for (i = 0; args[i]; i++)
-      printf("%s\n",args[i]);
-    
+    //printf("%s %d\n",args[0],strcmp(args[0],"exit"));
+    if (strcmp(args[0],"exit")==0) {
+      should_run = 0;
+    } else {
+      pid_t pid;
+      pid = fork();
+      if (pid < 0) {
+	fprintf(stderr, "Something went wrong with fork call\n");
+      } else if (pid == 0) {
+	//child process
+	if (execvp(args[0],args) < 0) {
+	  fprintf(stderr, "Something went wrong with execvp\n");
+	  return 1;
+	}
+      } else {
+	//parent
+	wait(NULL);
+      }
+    }
   }
   return 0;
 }
